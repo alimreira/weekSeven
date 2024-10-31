f057ccdfe8f7fce99e5ddd41d493f393940448ad
Halimat <feyikemipereia1@gmail.com>
2024-10-31 11:40:13 +0100

    week seven

---
+++ src/main/java/com/bakery/pastry/PastryApplication.java
@@ -0,0 +1,13 @@
+package com.bakery.pastry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakery/pastry/PastryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakery.pastry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
